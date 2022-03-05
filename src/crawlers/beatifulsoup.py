60ad3ba66a9918432b473c47fb6da41b8bd0625f
hans.loefflad <hans.loefflad@camao.one>
2022-03-05 13:18:19 +0100

    fixed error in complete links

--- src/crawlers/beatifulsoup.py
+++ src/crawlers/beatifulsoup.py
@@ -1,3 +1,6 @@
+import re
+from typing import List, Tuple
+
 from src.crawlers import Crawler
 
 from bs4 import BeautifulSoup
@@ -5,10 +8,47 @@ import urllib.request
 import json
 
 class BeatifulSoupCrawler(Crawler):
-    def run(self):
-        pass
+    def __init__(self, start_url: str = "https://www.heilbronn.de/sitemap.html", base_url: str = "https://www.heilbronn.de"):
+        self.start_url = start_url
+        self.base_url = base_url
+        self.websites = []
+        self.pdfs = []
 
-    def get_links(self, url):
+    @staticmethod
+    def _get_links(url):
         html_doc = urllib.request.urlopen(url)
         soup = BeautifulSoup(html_doc, 'html.parser')
-        return soup
+        filter_fn = lambda tag: (tag.name == "a") and (":" not in tag["href"]) and ("/" in tag["href"])
+        tags = soup.findAll(filter_fn)
+        links = [tag["href"] for tag in tags]
+
+        return links
+
+    def _complete_links(self, links: List[str]) -> List[str]:
+        complete_links = [f"{self.base_url}{link}" for link in links]
+
+        return complete_links
+
+    @staticmethod
+    def _separate_links(links: List[str]) -> Tuple[List[str], List[str]]:
+        websites, pdfs = [], []
+        for link in links:
+            if ".pdf" in link:
+                pdfs.append(link)
+            else:
+                websites.append(link)
+
+        return websites, pdfs
+
+    def _crawl_website(self, url):
+        links = self._get_links(url)
+        complete_links = self._complete_links(links)
+        websites, pdfs = self._separate_links(complete_links)
+        self.websites.extend(websites)
+        self.pdfs.extend(pdfs)
+
+        for website in websites:
+            self._crawl_website(website)
+
+    def run(self):
+        self._crawl_website(self.start_url)
