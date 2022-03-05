7fe2d2ed8665f68570db11f44bd73422e7e61c18
ksu <kevin.suedmersen@camao.one>
2022-03-05 17:30:51 +0100

    getting all information extraction classes

--- src/information_extraction_engine/runner.py
+++ src/information_extraction_engine/runner.py
@@ -6,34 +6,37 @@ from src.information_extraction_engine import InformationExtractor
 
 
 class InformationExtractionEngineRunner:
-    def _get_all_classes(self, filepath: str) -> List:
-        pass
+    def __init__(self, package_dirnanme: str = None):
+        if package_dirnanme is not None:
+            self.package_dirname = package_dirnanme
+        else:
+            self.package_dirname = os.path.dirname(__file__)
 
-    def _get_extractors(self, package_dirname: str) -> List:
-
-        for module_loader, name, is_package in pkgutil.iter_modules([package_dirname]):
+    def _get_package_classes(self) -> List:
+        """Import and return all class objects inside the package `package_name`"""
+        for module_loader, name, is_package in pkgutil.iter_modules([self.package_dirname]):
             importlib.import_module('.' + name, __package__)
-
         package_classes = InformationExtractor.__subclasses__()
 
+        return package_classes
 
-        filenames = os.listdir(package_dirname)
-        filepaths = [os.path.join(package_dirname, filename) for filename in filenames]
-        all_extractors = []
-        for filepath in filepaths:
-            extractors_in_file = self._get_extractors_from_file(filepath)
-            all_extractors.append(extractors_in_file)
+    def _get_extractors(self) -> List:
+        """Returns all classes inheriting from `InformationExtractor`"""
+        package_classes = self._get_package_classes()
+        information_extractors = [cls for cls in package_classes if issubclass(cls, InformationExtractor)]
 
-    def _separate_extractors(self):
+        return information_extractors
+
+    def _group_extractors(self):
         pass
 
     def _run_extractors(self):
         pass
 
     def run_engine(self):
-        information_extractors = self._get_extractors(os.path.dirname(__file__))
+        information_extractors = self._get_extractors()
         pre_processing_extractors, processing_extractors, post_processing_extractors = \
-            self._separate_extractors(information_extractors)
+            self._group_extractors(information_extractors)
         self._run_extractors(pre_processing_extractors)
         self._run_extractors(processing_extractors)
         self._run_extractors(post_processing_extractors)
