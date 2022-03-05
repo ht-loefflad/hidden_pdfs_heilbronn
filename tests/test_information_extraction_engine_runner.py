7f00bd16cfd66a586d4f5d57c1a60fa0fe9026fd
ksu <kevin.suedmersen@camao.one>
2022-03-05 21:23:43 +0100

    basic test

--- tests/test_information_extraction_engine_runner.py
+++ tests/test_information_extraction_engine_runner.py
@@ -5,8 +5,43 @@ from src.information_extraction_engine.runner import InformationExtractionEngine
 
 class TestInformationExtractionEngineRunner:
     @pytest.fixture
+    def json_doc(self):
+        return {
+            "Result": [
+                {
+                    "Metadata": {
+                        "Storagepath": "pdfs\\Wegweiser_bei_seelischen_Problemen.pdf"
+                    },
+                    "Pdflink": "https://www.heilbronn.de//fileadmin/daten/stadtheilbronn/formulare/leben/beratungsstellen/Wegweiser_bei_seelischen_Problemen.pdf",
+                    "Websitelink": "https://www.heilbronn.de//leben/gesundheit/beratungsstellen/ibb"
+                },
+                {
+                    "Metadata": {
+                        "Storagepath": "pdfs\\Beratungsstellen.pdf"
+                    },
+                    "Pdflink": "https://www.heilbronn.de//fileadmin/daten/stadtheilbronn/formulare/leben/beratungsstellen/Beratungsstellen.pdf",
+                    "Websitelink": "https://www.heilbronn.de//leben/gesundheit/beratungsstellen/ibb"
+                },
+                {
+                    "Metadata": {
+                        "Storagepath": "pdfs\\HN_Innenstadt_Broschuere_7hEStG.pdf"
+                    },
+                    "Pdflink": "https://www.heilbronn.de//fileadmin/daten/stadtheilbronn/formulare/bauen_wohnen/Stadterneuerung/HN_Innenstadt_Broschuere_7hEStG.pdf",
+                    "Websitelink": "https://www.heilbronn.de//stadterneuerung.html"
+                },
+                {
+                    "Metadata": {
+                        "Storagepath": "pdfs\\HN_Innenstadt_Broschuere_7hEStG.pdf"
+                    },
+                    "Pdflink": "https://www.heilbronn.de//fileadmin/daten/stadtheilbronn/formulare/bauen_wohnen/Stadterneuerung/HN_Innenstadt_Broschuere_7hEStG.pdf",
+                    "Websitelink": "https://www.heilbronn.de//stadterneuerung.html"
+                }
+            ]
+        }
+
+    @pytest.fixture
     def iee_runner(self):
         return InformationExtractionEngineRunner()
 
-    def test_run_engine(self, iee_runner):
-        iee_runner.run_engine()
+    def test_run_engine(self, iee_runner, json_doc):
+        iee_runner.run_engine(json_doc)
