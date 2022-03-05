8a9b283990bc935ca443d065bb091524e86f6d4b
ksu <kevin.suedmersen@camao.one>
2022-03-05 17:58:14 +0100

    able to group different processing types

--- src/information_extraction_engine/runner.py
+++ src/information_extraction_engine/runner.py
@@ -1,8 +1,8 @@
 import os
 import pkgutil
 import importlib
-from typing import List
-from src.information_extraction_engine import InformationExtractor
+from typing import List, Tuple
+from src.information_extraction_engine import InformationExtractor, ProcessingType
 
 
 class InformationExtractionEngineRunner:
@@ -20,23 +20,41 @@ class InformationExtractionEngineRunner:
 
         return package_classes
 
-    def _get_extractors(self) -> List:
+    def _get_information_extractors(self) -> List:
         """Returns all classes inheriting from `InformationExtractor`"""
         package_classes = self._get_package_classes()
         information_extractors = [cls for cls in package_classes if issubclass(cls, InformationExtractor)]
 
         return information_extractors
 
-    def _group_extractors(self):
-        pass
+    def _group_information_extractors(self, information_extractors: List) -> Tuple[List, List, List]:
+        """Groups extractors into pre-processing, processing and post-processing extractors"""
+        pre_processing_extractors = []
+        processing_extractors = []
+        post_processing_extractors = []
+
+        for information_extractor in information_extractors:
+            if information_extractor.TYPE == ProcessingType.PreProcessing:
+                pre_processing_extractors.append(information_extractor)
+
+            elif information_extractor.TYPE == ProcessingType.Processing:
+                processing_extractors.append(information_extractor)
+
+            elif information_extractor.TYPE == ProcessingType.PostProcessing:
+                post_processing_extractors.append(information_extractor)
+
+            else:
+                raise ValueError(f"Unknown PreprocessingType provided: {information_extractor.TYPE}")
+
+        return pre_processing_extractors, processing_extractors, post_processing_extractors
 
     def _run_extractors(self):
         pass
 
     def run_engine(self):
-        information_extractors = self._get_extractors()
+        information_extractors = self._get_information_extractors()
         pre_processing_extractors, processing_extractors, post_processing_extractors = \
-            self._group_extractors(information_extractors)
+            self._group_information_extractors(information_extractors)
         self._run_extractors(pre_processing_extractors)
         self._run_extractors(processing_extractors)
         self._run_extractors(post_processing_extractors)
